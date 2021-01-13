b991b9b148bd6bae0af7784fef8e6ebbdf86c6fe
Minh Nguyen <mnguyen0226@vt.edu>
2021-01-13 17:24:17 -0500

    Minor Change

--- Foundations of Deep RL/Chapter 2: Reinforce/basic_RL/basic_RL_CartPole.py
+++ Foundations of Deep RL/Chapter 2: Reinforce/basic_RL/basic_RL_CartPole.py
@@ -30,12 +30,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-# 1/ Initialize learning rate
+# Initialize learning rate
 gamma = 0.99
 
-
+# Class for Monte Carlo Randomization
 class Pi(nn.Module):
-    def __init__(self, in_dim, out_dim):
+    def __init__(self, in_dim, out_dim): # initialize network, model, reset state, and training mode
         super(Pi, self).__init__()
         layers = [
             nn.Linear(in_dim, 64),
@@ -46,11 +46,11 @@ class Pi(nn.Module):
         self.onpolicy_reset()
         self.train()  # set training mode
 
-    def onpolicy_reset(self):
+    def onpolicy_reset(self): # Clear up log prob and rewards since data is unreuseable
         self.log_probs = []
         self.rewards = []
 
-    def forward(self, x):
+    def forward(self, x): # Forward pass
         pdparam = self.model(x)
         return pdparam
 
@@ -74,6 +74,7 @@ def train(pi, optimizer):
     for t in reversed(range(T)):
         future_ret = pi.rewards[t] + gamma * future_ret
         rets[t] = future_ret
+
     rets = torch.tensor(rets)
     log_probs = torch.stack(pi.log_probs)
     loss = -log_probs * rets  # gradient terms; Negative for maximizing
@@ -83,9 +84,8 @@ def train(pi, optimizer):
     optimizer.step()  # gradient-ascent, updates the weights
     return loss
 
-
 def main():
-    env = gym.make("CartPole-v0")
+    env = gym.make("CartPole-v0") # Create environment
     in_dim = env.observation_space.shape[0]
     out_dim = env.action_space.n
     pi = Pi(in_dim, out_dim)  # policy pi_theta for Reinforce
@@ -103,12 +103,11 @@ def main():
         loss = train(pi, optimizer)  # train per episode
 
         total_reward = sum(pi.rewards)
-        solved = total_reward > 195.0
+        solved = total_reward > 195.0 # solve the cartpole if the total reward
         pi.onpolicy_reset()  # clear memory after training
         print(
             f"Episode {epi}, loss: {loss}, \ total_reward: {total_reward}, solved: {solved}"
         )
-
 
 if __name__ == "__main__":
     main()
